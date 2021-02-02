c75f94d57bfab63e6e861fecee4449de94831b97
Jiří Vrba <jiri@vrba.dev>
2021-02-02 22:57:05 +0100

    Intial commit, fresh spring boot installation

---
+++ src/test/java/dev/vrba/studentskyportal/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.studentskyportal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
