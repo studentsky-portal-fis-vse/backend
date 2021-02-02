9f1802e32c8ed5b1fdc1767b7a91ae13815eca0b
Jiří Vrba <jiri@vrba.dev>
2021-02-02 23:24:09 +0100

    Default bean for password encoder 🔐

--- src/main/java/dev/vrba/studentskyportal/backend/security/SecurityConfiguration.java
+++ src/main/java/dev/vrba/studentskyportal/backend/security/SecurityConfiguration.java
@@ -1,9 +1,14 @@
 package dev.vrba.studentskyportal.backend.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -24,5 +29,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // Disable storing session and appending the JSESSIONID cookie as JWT is stateless by design
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
