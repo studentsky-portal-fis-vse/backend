15f6a1a7d23df31600efce09607d9322497f9a21
Jiří Vrba <jiri@vrba.dev>
2021-02-03 12:22:05 +0100

    Implemented AuthenticationController.login with UsersService

--- src/main/java/dev/vrba/studentskyportal/backend/controllers/AuthenticationController.java
+++ src/main/java/dev/vrba/studentskyportal/backend/controllers/AuthenticationController.java
@@ -2,18 +2,33 @@ package dev.vrba.studentskyportal.backend.controllers;
 
 import dev.vrba.studentskyportal.backend.requests.authentication.LoginRequest;
 import dev.vrba.studentskyportal.backend.requests.authentication.RegistrationRequest;
+import dev.vrba.studentskyportal.backend.services.UsersService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/authentication")
 public class AuthenticationController {
 
+    private final UsersService usersService;
+
+    public AuthenticationController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.CREATED)
-    public void login(@Valid @RequestBody LoginRequest request) {
+    public Map<String, String> login(@Valid @RequestBody LoginRequest request) {
+        String token = usersService.loginUserAndObtainToken(request.getUsername(), request.getPassword());
+
+        Map<String, String> response = new HashMap<>();
+        response.put("token", token);
+
+        return response;
     }
 
     @PostMapping("/registration")
